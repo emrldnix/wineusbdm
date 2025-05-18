4b9273475a83e8f97112849cd09fe1ab44bec19f
Ryan Omasta <git@ryand.ca>
2025-05-17 20:54:52 -0600

    chore(flake): update flake.lock

--- flake.nix
+++ flake.nix
@@ -2,8 +2,7 @@
   description = "wineusbdm";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
-    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
 
     usbdm-flake = {
       url = "github:ryand56/usbdm-nix";
@@ -15,7 +14,6 @@
     {
       self,
       nixpkgs,
-      nixpkgs-unstable,
       usbdm-flake,
     }:
     let
@@ -24,33 +22,15 @@
         "i686-linux"
       ];
       overlay = final: prev: {
-        haskellPackages = prev.haskell.packages.ghc924Binary.override (old: {
-          overrides = final.lib.composeExtensions (old.overrides or (_: _: { })) (
-            hself: hsuper: {
-              unordered-containers = hself.callHackage "unordered-containers" "0.2.19.1" { };
-            }
-          );
-        });
-
         wineusbdm = final.pkgsi686Linux.callPackage ./default.nix {
           usbdm = usbdm-flake.packages.i686-linux.usbdm;
         };
       };
       forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f system);
-      forAllSystemsUnstable = f: nixpkgs-unstable.lib.genAttrs systems (system: f system);
 
       nixpkgsFor = forAllSystems (
         system:
         import nixpkgs {
-          inherit system;
-          overlays = [ self.overlay ];
-          config.allowUnfree = true;
-        }
-      );
-
-      nixpkgsForUnstable = forAllSystemsUnstable (
-        system:
-        import nixpkgs-unstable {
           inherit system;
           overlays = [ self.overlay ];
           config.allowUnfree = true;
@@ -61,7 +41,7 @@
       inherit overlay;
       defaultPackage = forAllSystems (system: nixpkgsFor.${system}.wineusbdm);
 
-      formatter = forAllSystemsUnstable (system: nixpkgsForUnstable.${system}.nixfmt-rfc-style);
+      formatter = forAllSystems (system: nixpkgsFor.${system}.nixfmt-rfc-style);
 
       packages = forAllSystems (system: {
         inherit (nixpkgsFor.${system}) wineusbdm;
